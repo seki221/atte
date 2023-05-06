35af7bc4a8de3f21a4332111e4867f264b42360f
seki221 <pawls00.bugaboo@icloud.com>
2023-05-06 20:15:57 +0900

    view変更

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -81,33 +81,6 @@
     </div>
   </div>
 
-  <div class="main__attendance">
-    <div class="attendance__left">
-      <fieldset id="field_ws">
-        @if(($isWorkStarted) || ($isWorkEnded))
-        <form action="/workStart" method="POST" class="timestamp">
-          @csrf
-          <button id="workstart" onclick='field_ws.disabled = false; workstart.disabled = true; workend.disabled = false;'> 勤務開始 </button>
-        </form>
-        @else
-        <form action="/workStart" method="POST" class="timestamp">
-          @csrf
-          <button class="button1">勤務開始</button>
-        </form>
-        @endif
-      </fieldset>
-      <fieldset id="field_rs">
-        <button id="reststart" onclick='field_rs.disabled = true; restend.disabled = false;'>休憩開始</button>
-      </fieldset>
-    </div>
-    <div class="attendance__right">
-      <fieldset id="field_we">
-        <button id="workend" disabled="true" onclick='field_rs.disabled = true; workstart.disabled = false; workend.disabled = true;'>勤務終了</button>
-      </fieldset>
-      <fieldset id="field_re">
-        <button id="restend" disabled="true" onclick='field_rs.disabled = false; restend.disabled = true;'>休憩終了</button>
-      </fieldset>
-    </div>
-  </div>
+  
 </main>
 @endsection

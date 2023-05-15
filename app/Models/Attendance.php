3e7b3c0ee9efec2edeca4d7dd71a55bd3f8ee607
seki221 <pawls00.bugaboo@icloud.com>
2023-05-15 19:15:04 +0900

    userの追加

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -5,11 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Rest;
+use App\Models\User;
 
 class Attendance extends Model
 {
     use HasFactory;
-
+    
+    public function user()
+    {
+        return $this->hasMany('App\Models\User');
+    }
+    
     public function rests()
     {
         return $this->hasMany('App\Models\Rest');
@@ -21,7 +27,7 @@ class Attendance extends Model
         'start_time',
         'end_time'
     ];
-
+    
     protected $hidden = [
         'created_at',
         'updated_at',

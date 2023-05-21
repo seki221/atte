90e880637c2cbfb1e0248113492346458df7d97b
seki221 <pawls00.bugaboo@icloud.com>
2023-05-21 22:17:31 +0900

    login画面とbackup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,64 @@
 @extends('layouts.default')
 
 <head>
-    <link rel="stylesheet" href="css/login.css">
+  <link rel="stylesheet" href="css/login.css">
 </head>
 
 @section('title', 'ログイン')
 @section('content')
 <x-guest-layout>
-    <x-auth-card>
-        <!-- <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot> -->
-        <div class="login-title">
-            <h1>ログイン</h1>
-        </div>
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+  <x-auth-card>
+    <div class="login-title">
+      <h1>ログイン</h1>
+    </div>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    <!-- Validation Errors -->
+    <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+    <form method="POST" action="{{ route('login') }}">
+      @csrf
 
-            <!-- Email Address -->
-            <div>
-                <!-- <x-label for="email" :value="__('Email')" /> -->
+      <!-- Email Address -->
+      <div>
+        <!-- <x-label for="email" :value="__('Email')" /> -->
 
-                <x-input id="email" class="block mt-1 w-full bg-gray-main" type="email" name="email" :value="old('email')" required autofocus placeholder="メールアドレス" />
-            </div>
+        <x-input id="email" class="block mt-1 w-full bg-gray-main" type="email" name="email" :value="old('email')" required autofocus placeholder="メールアドレス" />
+      </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <!-- <x-label for="password" :value="__('Password')" /> -->
+      <!-- Password -->
+      <div class="mt-4">
+        <!-- <x-label for="password" :value="__('Password')" /> -->
 
-                <x-input id="password" class="block mt-1 w-full bg-gray-main" type="password" name="password" required autocomplete="current-password" placeholder="パスワード" />
-            </div>
+        <x-input id="password" class="block mt-1 w-full bg-gray-main" type="password" name="password" required autocomplete="current-password" placeholder="パスワード" />
+      </div>
 
-            <!-- Remember Me -->
-            <!-- <div class="block mt-4">
+      <!-- Remember Me -->
+      <!-- <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div> -->
 
-            <div class="flex items-center justify-end mt-4">
-                <!-- @if (Route::has('password.request'))
+      <div class="flex items-center justify-end mt-4">
+        <!-- @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif -->
 
-                <!-- <x-button class="ml-3 button-login">
+        <!-- <x-button class="ml-3 button-login">
                     {{ __('Log in') }}
                 </x-button> -->
-                <button class=" button-login">ログイン</button>
-            </div>
-        </form>
-        <div class="link-register">
-            <p class="register-message">アカウントをお持ちでない方はこちらから</p>
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">会員登録</a>
-        </div>
-    </x-auth-card>
+        <button class=" button-login">ログイン</button>
+      </div>
+    </form>
+    <div class="link-register">
+      <p class="register-message">アカウントをお持ちでない方はこちらから</p>
+      <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">会員登録</a>
+    </div>
+  </x-auth-card>
 </x-guest-layout>
 @endsection

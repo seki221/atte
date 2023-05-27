ce90bc99e2daf53d81e7197ef82725f119ffb31f
seki221 <pawls00.bugaboo@icloud.com>
2023-05-27 22:58:51 +0900

    add user_page

---
+++ resources/views/user_page.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.default')
+
+<head>
+  <!-- <link rel="stylesheet" href="css/list.css"> -->
+  <link rel="stylesheet" href="css/user_page.css">
+</head>
+
+@section('title', 'ユーザー一覧')
+@section('content')
+<div class="main">
+  <div class="user-title">
+    <h1>ユーザー一覧</h1>
+  </div>
+  <div class="result user-list">
+    <table class="result-table user-table">
+      <tr class="table-title">
+        <th>名前</th>
+        <th>メールアドレス</th>
+      </tr>
+      @foreach ($users as $user)
+      <form action="/user_list" method="get">
+        <tr class="table-value table-value-user">
+          <td>
+            <button name="name"  value="{{ $user->name }}" class="name-button">{{ $user->name }}</button>
+          </td>
+          <td>{{ $user->email }}</td>
+        </tr>
+      </form>
+      @endforeach
+    </table>
+  </div>
+  <div class="paginate">
+    {{ $users->links() }}
+  </div>
+</div>
+  @endsection

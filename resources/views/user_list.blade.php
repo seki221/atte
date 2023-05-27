7aeaf9b943501ded12379eb3e52a40b89003787d
seki221 <pawls00.bugaboo@icloud.com>
2023-05-27 18:48:30 +0900

    add user_list

--- resources/views/user_list.blade.php
+++ resources/views/user_list.blade.php
@@ -3,25 +3,39 @@
 <head>
   <link rel="stylesheet" href="/css/reset.css">
   <link rel="stylesheet" href="/css/list.css">
+  <link rel="stylesheet" href="/css/user_list.css">
+
 
 </head>
-@section('title', '日付一覧')
+@section('title', 'ユーザー別勤怠ページ')
 @section('content')
 
 <main>
-  <div class="date">
-    <form action="/attendance_list" method="get">
-      @csrf
-      <button name="date" id="prev" value="{{ $subday }}"> &lt;
-      </button>
-    </form>
-    <p class="date__today">{{ $today }}</p>
-    <form action="/attendance_list" method="get">
-      @csrf
-      <button name="date" id="next" value="{{ $today }}"> &gt;</button>
-    </form>
+  <div class="user_name">
+    <p>{{ $userName }}さんの勤怠記録</p>
   </div>
 
+  <div class="date">
+    <form action="/user_page">
+      <label for="time">表示月の変更：</label>
+      <select name=" month" id="selectMonth">
+        <option value="">-</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+      </select>月
+    </form>
+    {{$attendances}}
+  </div>
   <div class="list">
     <table class="attandance_list">
       <tr class="table-title">
@@ -44,7 +58,7 @@
   </div>
   <div class="paginate">
     <form action="/attendance_list/{num}}" method="get">
-      <input type="hidden" name="date" value="{{ $today }}">
+      <input type="hidden" name="date" value="date">
       {{ $attendances->links() }}
     </form>
   </div>
